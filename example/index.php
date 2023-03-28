37d081be55b612c062f6e43593ef718e7f0aa70e
usualtool <52517593+usualtool@users.noreply.github.com>
2023-03-28 17:35:50 +0800

    Update index.php

--- example/index.php
+++ example/index.php
@@ -1,4 +1,5 @@
 <?php
 require dirname(__FILE__).'/'.'session.php';
-echo$openid;
-echo"<br/><a href='?m=service&p=login&do=out'>退出</a>";
+echo"用户唯一标志：".$openid."<br/>缓存用户数据：<br/>";
+print_r($data);
+echo"<br/><a href='?m=service&p=login&do=out'>退出应用</a>";
